83fc86e9d12662fd7dee8274aa196bcf0c19864f
DESKTOP-Q66EQI4\rs7 <rsolm72@gmail.com>
2018-07-05 23:24:28 -0600

    created Navigation Component and colored body a bit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Navigation />
       </div>
     );
   }
