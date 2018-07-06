55cdd09b71c3a80c49fd9884a6760c353e80a463
DESKTOP-Q66EQI4\rs7 <rsolm72@gmail.com>
2018-07-06 02:39:35 -0600

    created new Rank Component and added animated particles in background

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+            <div className="white f3">
+            <strong>{'Ruben your current rank is...'}</strong>
+            </div>
+            <div className="white f1">
+                <strong>{'#5'}</strong>
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
