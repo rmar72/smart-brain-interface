204ad534f7175f5dc21573cc115c208e810fb85a
DESKTOP-Q66EQI4\rs7 <rsolm72@gmail.com>
2018-07-07 19:50:45 -0600

    API set up initialization

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Clarifai from 'clarifai';
 import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: 'e335640338224000ba3d1826e422aedb'
+ });
 
 const particleOptions = {
   particles: {
