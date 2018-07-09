9febcb9312b26677756015c4cec62976d7f1c0db
DESKTOP-Q66EQI4\rs7 <rsolm72@gmail.com>
2018-07-08 20:48:10 -0600

    created new SignIn component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import SignIn from './components/SignIn/SignIn';
 import './App.css';
 
 const clarifaiApp = new Clarifai.App({
