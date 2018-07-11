cbfed2a3278b6c56638270a6ceb94a5c1e36136b
DESKTOP-Q66EQI4\rs7 <rsolm72@gmail.com>
2018-07-10 22:43:00 -0600

    new main app js state props, to be able to track users

--- src/App.js
+++ src/App.js
@@ -36,7 +36,14 @@ class App extends Component {
       input: '',
       imgUrl : '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      user: {
+        id:'',
+        name:'',
+        email:'',
+        entries:0,
+        joined: ''
+      }
     }
   }
 
