807ff50503874f0b46355c4208c5ae0f2b02b997
DESKTOP-Q66EQI4\rs7 <rsolm72@gmail.com>
2018-07-10 19:30:13 -0600

    new logic. upon button submit we fetch a Post req json-response from the smart-brain api, if succesful we call onRouteChange to switch view to 'home'

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -49,6 +49,12 @@ class SignIn extends React.Component {
     
                         </fieldset>
                         <div className="">
+                        <input 
+                            className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                            type="submit" 
+                            value="Sign in"
+                            onClick={this.onSubmitSignIn}
+                        />
                         </div>
                         <div className="lh-copy mt3">
                             <p 
