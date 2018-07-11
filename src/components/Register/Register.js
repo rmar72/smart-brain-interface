1d393567951f7603fbb79a6a0970ac2a576e7b42
DESKTOP-Q66EQI4\rs7 <rsolm72@gmail.com>
2018-07-10 22:42:04 -0600

    new event logic for state props name, email and password

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -11,6 +11,23 @@ class Register extends React.Component {
         }
     }
 
+    onNameChange = (event) => {
+        this.setState({name: event.target.value})
+    }
+
+    onEmailChange = (event) => {
+        this.setState({email: event.target.value})
+    }
+
+    onPasswordChange = (event) => {
+        this.setState({password: event.target.value})
+    }
+
+    onRegisterSubmit = () => {
+        console.log(this.state)
+        this.props.onRouteChange('home');
+    }
+
     render(){
         return (
             <article className=" pa3 mt5 br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
@@ -20,16 +37,43 @@ class Register extends React.Component {
                         <legend className="f2 fw6 ph0 mh0">Register</legend>
                         <div className="mt3">
                             <label className="db fw6 lh-copy f6" htmlFor="Name">Name</label>
+                            <input 
+                                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                type="text" 
+                                name="name"  
+                                id="name"
+                                onChange={this.onNameChange}
+                            />
                         </div>
                         <div className="mt3">
                             <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                            <input 
+                                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                type="email" 
+                                name="email-address"  
+                                id="email-address"
+                                onChange={this.onEmailChange}
+                            />
                         </div>
                         <div className="mv3">
                             <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                            <input 
+                                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                type="password" 
+                                name="password"  
+                                id="password"
+                                onChange={this.onPasswordChange}
+                            />
                         </div>
     
                         </fieldset>
                         <div className="">
+                        <input 
+                            className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                            type="submit" 
+                            value="Register"
+                            onClick={this.onRegisterSubmit}
+                        />
                         </div>
     
                     </div>
