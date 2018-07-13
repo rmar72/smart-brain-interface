fdf7901c1f1e352ac2e2f5ad257e0f6948e470f0
DESKTOP-Q66EQI4\rs7 <rsolm72@gmail.com>
2018-07-13 04:22:14 -0600

    error handling for promise within clarifai api call to update entries

--- src/App.js
+++ src/App.js
@@ -99,6 +99,7 @@ class App extends Component {
       })
         .then(response => response.json())
         .then(count => this.setState(Object.assign(this.state.user, {entries: count})))
+        .catch(err => console.log);
 
       this.faceBox( this.faceLocation(response) ) 
     })
